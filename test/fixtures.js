55d405c00df142c876fad97554eb59524c0fa76f
paullep <101050383+paullep@users.noreply.github.com>
2022-06-14 10:54:22 +0200

    Update fixtures.js

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,4 +1,4 @@
-export const serviceUrl = "http://localhost:4000";
+export const serviceUrl = "https://webbackendpaul.herokuapp.com";
 
 export const maggie = {
   firstName: "Maggie",
@@ -47,20 +47,24 @@ export const testCandidates = [
 export const testDonations = [
   {
     amount: 90,
-    method: "cash",
+    method: "Faculty",
     lat: "52.160858",
     lng: "-7.152420",
+    image: "https://www.centralesupelec.fr/sites/all/themes/cs_theme/medias/common/images/intro/logo.png https://www.centralesupelec.fr/sites/default/files/styles/1140x426/public/push-fr-campus_0.jpg?itok=qrsSmk_E https://detail-cdn.s3.eu-central-1.amazonaws.com/media/catalog/product/K/L/KLT-Lab-City-OMA-Philippe-Ruault.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=&width=",
   },
   {
     amount: 80,
-    method: "cash",
+    method: "Faculty",
     lat: "52.149220",
     lng: "-6.994620",
+    image: "https://www.bouygues-construction.com/sites/default/files/centralesupelec_saclay_bouygues_construction_1_28.jpg",
   },
   {
     amount: 50,
-    method: "paypal",
+    method: "Engineering School",
     lat: "52.161290",
     lng: "-7.231540",
+    image: "https://www.bouygues-construction.com/sites/default/files/centralesupelec_saclay_bouygues_construction_1_28.jpg https://www.centralesupelec.fr/sites/all/themes/cs_theme/medias/common/images/intro/logo.png https://www.centralesupelec.fr/sites/default/files/styles/1140x426/public/push-fr-campus_0.jpg?itok=qrsSmk_E https://detail-cdn.s3.eu-central-1.amazonaws.com/media/catalog/product/K/L/KLT-Lab-City-OMA-Philippe-Ruault.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=&width=",
   },
 ];
+
