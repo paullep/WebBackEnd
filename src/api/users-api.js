e4fd8b41b8e530e1a4a46d8adad11d65ecde6e87
paullep <101050383+paullep@users.noreply.github.com>
2022-06-15 08:24:22 +0200

    Update users-api.js

--- src/api/users-api.js
+++ src/api/users-api.js
@@ -58,9 +58,11 @@ export const userApi = {
         return Boom.serverUnavailable("Database Error");
       }
     },
-        tags: ["api"],
+    tags: ["api"],
     description: "Create a User",
     notes: "Returns the newly created user",
+    validate: { payload: UserSpec, failAction: validationError },
+    response: { schema: UserSpec, failAction: validationError },
   },
   
     deleteOne: {
@@ -89,6 +91,10 @@ export const userApi = {
         return Boom.serverUnavailable("Database Error");
       }
     },
+        tags: ["api"],
+    description: "Delete all userApi",
+    notes: "All userApi removed from Playtime",
+
   },
 
   authenticate: {
